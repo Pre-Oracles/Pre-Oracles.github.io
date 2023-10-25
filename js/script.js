bbc7b9704fcce4734fac4488d911ecb4ab8843b4
DeusInRegnium <70289813+Pre-Oracles@users.noreply.github.com>
2023-10-25 13:32:40 -0700

    New look

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(slideIndex) {
+    slides[currentSlide].classList.remove('active');
+    slides[slideIndex].classList.add('active');
+    currentSlide = slideIndex;
+}
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowUp' && currentSlide > 0) {
+        showSlide(currentSlide - 1);
+    } else if (e.key === 'ArrowDown' && currentSlide < slides.length - 1) {
+        showSlide(currentSlide + 1);
+    }
+});
+
+// Set the initial slide to be active
+slides[currentSlide].classList.add('active');
