3993c561e706d3b73bdb3f21c1ba553875b4e5f2
DeusInRegnium <70289813+Pre-Oracles@users.noreply.github.com>
2023-10-26 23:06:54 -0700

    more mobile

--- js/script.js
+++ js/script.js
@@ -12,18 +12,30 @@ document.addEventListener('touchstart', (e) => {
     touchstartY = e.changedTouches[0].screenY;
 });
 
-document.addEventListener('touchend', (e) => {
-    touchendY = e.changedTouches[0].screenY;
+document.addEventListener('touchstart', (e) => {
+    if (isNavigating) return; // Ignore touch events while navigating
+    isNavigating = true;
 
-    // Determine the direction of the tap
-    const deltaY = touchendY - touchstartY;
-    if (deltaY < -50 && currentSlide > 0) {
-        // Swipe up (move to previous slide)
-        showSlide(currentSlide - 1);
-    } else if (deltaY > 50 && currentSlide < slides.length - 1) {
-        // Swipe down (move to next slide)
-        showSlide(currentSlide + 1);
-    }
+    const touchstartY = e.changedTouches[0].screenY;
+
+    document.addEventListener('touchend', function touchEndHandler(e) {
+        document.removeEventListener('touchend', touchEndHandler);
+
+        const touchendY = e.changedTouches[0].screenY;
+
+        // Determine the direction of the tap
+        const deltaY = touchendY - touchstartY;
+
+        if (deltaY < -50 && currentSlide > 0) {
+            // Swipe up (move to previous slide)
+            showSlide(currentSlide - 1);
+        } else if (deltaY > 50 && currentSlide < slides.length - 1) {
+            // Swipe down (move to next slide)
+            showSlide(currentSlide + 1);
+        }
+
+        isNavigating = false; // Reset the navigation flag
+    });
 });
 
 slides[currentSlide].classList.add('active');
