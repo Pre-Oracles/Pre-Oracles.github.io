1305a1e82749c2e1cd0be9e76e6bf8992f460549
DeusInRegnium <70289813+Pre-Oracles@users.noreply.github.com>
2023-10-26 23:18:14 -0700

    End

--- js/script.js
+++ js/script.js
@@ -3,26 +3,38 @@ let currentSlide = 0;
 
 
 
-document.addEventListener('click', function handleClick(event) {
-    if (isNavigating) return; // Ignore clicks while navigating
-    isNavigating = true;
-
-    setTimeout(() => {
-        const tapY = event.clientY;
-
-        if (tapY < window.innerHeight / 2 && currentSlide > 0) {
-            // Tap on the upper half of the screen (move to previous slide)
-            showSlide(currentSlide - 1);
-        } else if (tapY >= window.innerHeight / 2 && currentSlide < slides.length - 1) {
-            // Tap on the lower half of the screen (move to next slide)
-            showSlide(currentSlide + 1);
-        }
-
-        isNavigating = false; // Reset the navigation flag
-    }, 300); // Adjust the timeout as needed to control the delay
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowUp' && currentSlide > 0) {
+        showSlide(currentSlide - 1);
+    } else if (e.key === 'ArrowDown' && currentSlide < slides.length - 1) {
+        showSlide(currentSlide + 1);
+    }
 });
 
 
+let touchstartX = 0;
+let touchendX = 0;
+
+document.addEventListener('touchstart', (e) => {
+    touchstartX = e.changedTouches[0].screenX;
+});
+
+document.addEventListener('touchend', (e) => {
+    touchendX = e.changedTouches[0].screenX;
+    handleGesture();
+});
+
+function handleGesture() {
+    if (touchendX < touchstartX) {
+        // Swipe left (move to next slide)
+        showSlide(currentSlide + 1);
+    }
+
+    if (touchendX > touchstartX) {
+        // Swipe right (move to previous slide)
+        showSlide(currentSlide - 1);
+    }
+}
 
 slides[currentSlide].classList.add('active');
 
