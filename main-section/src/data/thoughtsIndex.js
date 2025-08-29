e1002d27611628f64f0bcbf764f9d5c0108cbff4
alexgaoth <alexisobrenovic@gmail.com>
2025-08-29 15:49:02 -0700

    update

--- main-section/src/data/thoughtsIndex.js
+++ main-section/src/data/thoughtsIndex.js
@@ -19,24 +19,23 @@ export const thoughtsIndex = [
       articleFile: manzikert
     },
     { 
-        title: "Chinese nationalization modernization is actually quite intersting", 
-        date: "2024-09-03", 
-        excerpt: "Chinese intellectuals simultaneously had to invent the concept of China as a modern nation-state while defending it against foreign encroachment, essentially creating and protecting a national identity in the same historical moment. This type of attempts to speedrun modernization is so chaotic, and thus interesting. Similiar situation can be seen with the Ottoman Empire before WW1" ,
-        image: "/resources/yatsen.jpg",
-        readTime: calculateReadTime(china),
-        tags: ["chinese","nationalism", "colonialism"],
-        articleFile: china
-      },
-      { 
-        title: "The Future of AI Agents", 
-        date: "2024-08-01", 
-        image: "/resources/ai_agent.png",
-        excerpt: "It is not just going to be transformers, because whenever a technology is only being slightly refined under heavy competition, it suggest the end is near for it (even if it takes 50 years), new paradigms, new technology will emerge eventually and overtake transformers like how they overtook the CNNs and their predecessors. " ,
-        readTime: calculateReadTime(AI),
-        tags: ["tech", "ML","DL","Transformers"],
-        articleFile: AI
-      }
-  
+      title: "Chinese nationalization modernization is actually quite intersting", 
+      date: "2024-09-03", 
+      excerpt: "Chinese intellectuals simultaneously had to invent the concept of China as a modern nation-state while defending it against foreign encroachment, essentially creating and protecting a national identity in the same historical moment. This type of attempts to speedrun modernization is so chaotic, and thus interesting. Similiar situation can be seen with the Ottoman Empire before WW1" ,
+      image: "/resources/yatsen.jpg",
+      readTime: calculateReadTime(china),
+      tags: ["chinese","nationalism", "colonialism"],
+      articleFile: china
+    },
+    { 
+      title: "The Future of AI Agents", 
+      date: "2024-08-01", 
+      image: "/resources/ai_agent.png",
+      excerpt: "It is not just going to be transformers, because whenever a technology is only being slightly refined under heavy competition, it suggest the end is near for it (even if it takes 50 years), new paradigms, new technology will emerge eventually and overtake transformers like how they overtook the CNNs and their predecessors. " ,
+      readTime: calculateReadTime(AI),
+      tags: ["tech", "ML","DL","Transformers"],
+      articleFile: AI
+    }
 ];
 export const getLatestThought = () => {
   return thoughtsIndex[0]; 
